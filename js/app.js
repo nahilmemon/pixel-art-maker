2e32242cc7f24f1acbb07fe7796ab9fc10eafa74
Nahil Memon <nahilmemon@hotmail.com>
2018-03-29 01:27:14 +0400

    feat: Implement the makeGrid() function to make a new canvas
    The makeGrid() function deletes the old table, reads the grid size inputs, and creates a new table accordingly.

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// Make the canvas grid in the format of a table
+function makeGrid() {
+  // Delete the previously made grid
+  const canvasTable = document.querySelector('#pixelCanvas');
+  canvasTable.innerHTML = '';
+
+  // Determine grid height and width inputs
+  const gridHeight = document.querySelector('#inputGridHeight').value;
+  const gridWidth = document.querySelector('#inputGridWidth').value;
+
+  // create a DocumentFragment to store the table contents
+  const fragment = document.createDocumentFragment();
+
+  // Make the canvas table and add it the document fragment
+  for (var row=0; row < gridWidth; row++) {
+    // Make a table row
+    const tableRow = document.createElement('tr');
+    for (var col=0; col <gridHeight; col++) {
+      // Make a table column
+      const tableCol = document.createElement('td');
+      // Add this table column to the row
+      tableRow.appendChild(tableCol);
+    }
+    // Add this row to the fragment
+    fragment.appendChild(tableRow);
+  }
+
+  // Add the table contents fragment to the table
+  canvasTable.appendChild(fragment);
+}
